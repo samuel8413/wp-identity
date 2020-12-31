a5e4bfad2223c39cf91addb06e9997d007c00b00
Samuel Lucio García Hidalgo <samuelluciogar@Samuels-MacBook-Air.local>
2020-12-31 11:59:54 -0600

    Add primary menu structure

---
+++ template-parts/utilities/navbar-primary.php
@@ -0,0 +1,13 @@
+<?php 
+
+wp_nav_menu(
+    array(
+      'menu'            => 'ul',
+      'theme_location'  => 'primary',
+      'container'       => 'nav',
+      'container_class' => 'navbar-primary-menu',
+      'menu_class'      => 'primary-menu',
+    )
+);
+
+?>
