cb44c0168d14f504757154c8d64971835f9d0ac1
Samuel Lucio García Hidalgo <samuelluciogar@Samuels-MacBook-Air.local>
2020-12-31 14:37:00 -0600

    Include header.php

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="<?php language_attributes(); ?>">
+
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <header>
+        <!-- Navbar -->
+        <?php get_template_part("template-parts/utilities/navbar", "primary") ?>
+    </header>
+
+    <main>
