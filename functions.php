781ba40c662b922613aa8f5377cd5493f9238feb
Samuel Lucio García Hidalgo <samuelluciogar@Samuels-MacBook-Air.local>
2020-12-31 11:34:39 -0600

    Register Side Bar

--- functions.php
+++ functions.php
@@ -78,3 +78,26 @@ function add_identity_scripts() {
 	}
 }
 add_action( 'wp_enqueue_scripts', 'add_identity_scripts' );
+
+/**************************************************************************************************
+ Register Side Bar
+**************************************************************************************************/
+
+function identity_widgets_init() {
+    register_sidebar(
+        array(
+            'id'            => 'primary',
+            'name'          => __( 'Primary Sidebar' ),
+            'description'   => __( 'A short description of the sidebar.' ),
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h3 class="widget-title">',
+            'after_title'   => '</h3>',
+        )
+    );
+}
+add_action( 'widgets_init', 'identity_widgets_init' );
+
+/**************************************************************************************************
+ Register Side Bar
+**************************************************************************************************/
