6ea6c7df40656003ec4fc7bb4a58366c6c8c48ba
Samuel Lucio García Hidalgo <samuelluciogar@Samuels-MacBook-Air.local>
2020-12-31 10:36:22 -0600

    functions.php basic setup

--- functions.php
+++ functions.php
@@ -1,8 +1,62 @@
 <?php
 
-function add_theme_scripts(){
-    wp_enqueue_style("style", get_stylesheet_uri());
-    wp_enqueue_script("app-js", get_bloginfo("stylesheet_directory") . "/assets/js/identity.bundle.js");
-}
+/**
+ * Identity functions and definitions
+ *
+ * @package Identity
+ * @since Identity 1.0
+ */
+ 
+/**************************************************************************************************
+ Content Width
+**************************************************************************************************/
 
-add_action("wp_enqueue_scripts", "add_theme_scripts");
+if ( ! isset( $content_width ) )
+    $content_width = 800; /* pixels */
+
+/**************************************************************************************************
+ Theme Setup
+**************************************************************************************************/
+
+if ( ! function_exists( 'identity_setup' ) ) :
+    /**
+     * Sets up theme defaults and registers support for various WordPress features.
+     *
+     * Note that this function is hooked into the after_setup_theme hook, which runs
+     * before the init hook. The init hook is too late for some features, such as indicating
+     * support post thumbnails.
+     */
+
+    function identity_setup() {
+        /**
+         * Make theme available for translation.
+         * Translations can be placed in the /languages/ directory.
+         */
+        load_theme_textdomain( 'identity', get_template_directory() . '/languages' );
+
+        /**
+         * Add default posts and comments RSS feed links to <head>.
+         */
+        add_theme_support( 'automatic-feed-links' );
+
+        /**
+         * Enable support for post thumbnails and featured images.
+         */
+        add_theme_support( 'post-thumbnails' );
+
+        /**
+         * Add support for custom navigation menus.
+         */
+        register_nav_menus( array(
+            'primary'   => __( 'Primary Menu', 'identity' ),
+            'secondary' => __( 'Secondary Menu', 'identity' )
+        ) );        
+
+        /**
+         * Enable support for the following post formats:
+         * aside, gallery, quote, image, and video
+         */
+        add_theme_support( 'post-formats',  array ( 'aside', 'gallery', 'quote', 'image', 'video' ) );
+    }
+endif; //identity_setup
+add_action( 'after_setup_theme', 'identity_setup' );
